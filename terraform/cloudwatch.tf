f1178e57bd9fcac463add61479eb53f44b8e23dd
eyupaksu-nhs <eyup.aksu@thoughtworks.com>
2022-04-13 16:21:32 +0100

    [PRMT-2604] Remove redundant health-metric alarm

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -10,21 +10,3 @@ resource "aws_cloudwatch_log_group" "log_group" {
     CreatedBy= var.repo_name
   }
 }
-
-resource "aws_cloudwatch_metric_alarm" "health_metric_failure_alarm" {
-  alarm_name                = "${var.environment}-${var.component_name}-health-metric-failure"
-  comparison_operator       = "LessThanThreshold"
-  threshold                 = "1"
-  evaluation_periods        = "3"
-  metric_name               = "Health"
-  namespace                 = local.gp_registrations_mi_forwarder_metric_namespace
-  alarm_description         = "Alarm to flag failed health checks"
-  statistic                 = "Maximum"
-  treat_missing_data        = "breaching"
-  period                    = "60"
-  dimensions = {
-    "Environment" = var.environment
-  }
-  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
-}
-
