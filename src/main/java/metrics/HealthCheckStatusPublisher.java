42f6693498ac904ea6f966195cc3a4b6f16f3796
eyupaksu-nhs <eyup.aksu@thoughtworks.com>
2022-04-14 10:20:54 +0100

    [PRMT-2604] Introduced health metric and alarm for that.

---
+++ src/main/java/metrics/HealthCheckStatusPublisher.java
@@ -0,0 +1,38 @@
+package metrics;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Slf4j
+public class HealthCheckStatusPublisher {
+
+    private static final int SECONDS = 1000;
+    private static final int MINUTE_INTERVAL = 60 * SECONDS;
+    public static final String HEALTH_METRIC_NAME = "Health";
+
+    private final MetricPublisher metricPublisher;
+
+    @Autowired
+    public HealthCheckStatusPublisher(MetricPublisher metricPublisher) {
+        this.metricPublisher = metricPublisher;
+    }
+
+    @Scheduled(fixedRate = MINUTE_INTERVAL)
+    public void publishHealthStatus() {
+        if (allProbesHealthy()) {
+            metricPublisher.publishMetric(HEALTH_METRIC_NAME, 1.0);
+        } else {
+            metricPublisher.publishMetric(HEALTH_METRIC_NAME, 0.0);
+        }
+    }
+
+    private boolean allProbesHealthy() {
+        //until adding  some other component we can initially return true.
+        return true;
+    }
+
+}
---
+++ src/test/java/metrics/HealthCheckStatusPublisherTest.java
@@ -0,0 +1,29 @@
+package metrics;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class HealthCheckStatusPublisherTest {
+    private MetricPublisher metricPublisher;
+
+    @BeforeEach
+    void setUp(){
+        metricPublisher = Mockito.mock(MetricPublisher.class);
+    }
+
+    @Test
+    public void shouldSetHealthMetricToHealthy() {
+
+        HealthCheckStatusPublisher healthPublisher = new HealthCheckStatusPublisher(metricPublisher);
+        healthPublisher.publishHealthStatus();
+
+        verify(metricPublisher,times(1)).publishMetric("Health", 1.0);
+    }
+
+}
