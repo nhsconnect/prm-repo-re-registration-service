3b5e15fd8dd14e1ae07b5008060ee59b8c16b2bd
romanenhs <romane.castera1@nhs.net>
2022-06-28 11:43:53 +0100

    [PRMT-2657] Created NonSensitiveDataMessage class for messages that only contain a status

---
+++ src/main/java/uk/nhs/prm/repo/re_registration/NonSensitiveDataMessage.java
@@ -0,0 +1,18 @@
+package uk.nhs.prm.repo.re_registration;
+
+import com.google.gson.GsonBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class NonSensitiveDataMessage {
+
+    private final String nemsMessageId;
+    private final String status;
+
+    public String toJsonString() {
+        return new GsonBuilder().disableHtmlEscaping().create()
+                .toJson(this);
+    }
+}
