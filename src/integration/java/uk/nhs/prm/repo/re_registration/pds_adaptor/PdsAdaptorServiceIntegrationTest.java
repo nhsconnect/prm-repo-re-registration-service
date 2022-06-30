d48fcd1ad0d5e6af2fec39caecf9175d1c879b57
syedsadiqali <syed.ali69@nhs.net>
2022-06-30 16:29:50 +0100

    [PRMT-2657] - Added Integration test for when pds adaptor responds with 500 status code

---
+++ src/integration/java/uk/nhs/prm/repo/re_registration/pds_adaptor/PdsAdaptorServiceIntegrationTest.java
@@ -0,0 +1,136 @@
+package uk.nhs.prm.repo.re_registration.pds_adaptor;
+
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.model.GetQueueAttributesResult;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.nhs.prm.repo.re_registration.infra.LocalStackAwsConfig;
+import uk.nhs.prm.repo.re_registration.model.ReRegistrationEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+@SpringBootTest()
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration( classes = LocalStackAwsConfig.class)
+public class PdsAdaptorServiceIntegrationTest {
+
+    public static final String NHS_NUMBER = "1234567890";
+    @Autowired
+    private AmazonSQSAsync sqs;
+
+    @Value("${aws.reRegistrationsQueueName}")
+    private String reRegistrationsQueueName;
+
+    private String splunkAuditUploader = "splunk-audit-uploader";
+
+    WireMockServer stubPdsAdaptor;
+    private String reRegistrationsQueueUrl;
+    private String splunkAuditUploaderUrl;
+
+
+    @BeforeEach
+    public void setUp() {
+        stubPdsAdaptor = initializeWebServer();
+        reRegistrationsQueueUrl = sqs.getQueueUrl(reRegistrationsQueueName).getQueueUrl();
+        splunkAuditUploaderUrl = sqs.getQueueUrl(splunkAuditUploader).getQueueUrl();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        stubPdsAdaptor.resetAll();
+        stubPdsAdaptor.stop();
+    }
+
+    private WireMockServer initializeWebServer() {
+        final WireMockServer wireMockServer = new WireMockServer(8080);
+        wireMockServer.start();
+        return wireMockServer;
+    }
+
+    @Test
+    void shouldNotPutAnythingOnReRegistrationAuditTopicWhenPdsReturnsResponseWithStatusCode500() {
+        setPds500ErrorState(NHS_NUMBER);
+        sqs.sendMessage(reRegistrationsQueueUrl,getReRegistrationEvent().toJsonString());
+        await().atMost(20, TimeUnit.SECONDS).untilAsserted(()->assertThat(isQueueEmpty(splunkAuditUploaderUrl)));
+    }
+
+    @Test
+    void shouldPutTheAuditStatusMessageOnAuditTopicWhenPdsReturnsResponseWithStatusCode200() {
+        setPds200SuccessState(NHS_NUMBER);
+
+        sqs.sendMessage(reRegistrationsQueueUrl,getReRegistrationEvent().toJsonString());
+        await().atMost(20, TimeUnit.SECONDS).untilAsserted(()->assertThat(checkMessageInRelatedQueue(splunkAuditUploaderUrl)));
+    }
+
+
+    private void setPds500ErrorState(String nhsNumber) {
+        stubFor(get(urlMatching("/suspended-patient-status/" + nhsNumber))
+                .withHeader("Authorization", matching("Basic cmUtcmVnaXN0cmF0aW9uLXNlcnZpY2U6ZGVmYXVsdA=="))
+                .willReturn(aResponse()
+                        .withStatus(500)// request unsuccessful with status code 500
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("<response>Some content</response>")));
+    }
+
+    private void setPds200SuccessState( String nhsNumber) {
+        stubFor(get(urlMatching("/suspended-patient-status/" + nhsNumber))
+                .withHeader("Authorization", matching("Basic cmUtcmVnaXN0cmF0aW9uLXNlcnZpY2U6ZGVmYXVsdA=="))
+                .willReturn(aResponse()
+                        .withStatus(200)// request unsuccessful with status code 500
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody(getPdsResponseString().getBody())));
+    }
+
+    private ReRegistrationEvent getReRegistrationEvent() {
+        return new ReRegistrationEvent(NHS_NUMBER, "ABC123", "nemsMessageId", "2017-11-01T15:00:33+00:00");
+    }
+
+    private boolean isQueueEmpty(String queueUrl) {
+        List<String> attributeList = new ArrayList<>();
+        attributeList.add("ApproximateNumberOfMessagesNotVisible");
+        attributeList.add("ApproximateNumberOfMessages");
+        GetQueueAttributesResult getQueueAttributesResult = sqs.getQueueAttributes(queueUrl, attributeList);
+
+        var numberOfMessageNotVisible = Integer.valueOf(getQueueAttributesResult.getAttributes().get("ApproximateNumberOfMessagesNotVisible"));
+        var numberOfMessageVisible = Integer.valueOf(getQueueAttributesResult.getAttributes().get("ApproximateNumberOfMessages"));
+
+        return (numberOfMessageVisible == 0 && numberOfMessageNotVisible == 0);
+    }
+
+    private List<Message> checkMessageInRelatedQueue(String queueUrl) {
+        System.out.println("checking sqs queue: " + queueUrl);
+
+        var requestForMessagesWithAttributes
+                = new ReceiveMessageRequest().withQueueUrl(queueUrl)
+                .withMessageAttributeNames("traceId");
+        List<Message> messages = sqs.receiveMessage(requestForMessagesWithAttributes).getMessages();
+        System.out.printf("Found %s messages on queue: %s%n", messages.size(), queueUrl);
+        assertThat(messages).hasSize(1);
+        return messages;
+    }
+
+    private ResponseEntity<String> getPdsResponseString() {
+        var pdsResponseString = "{\"nhsNumber\":\"" + NHS_NUMBER + "\",\"isSuspended\":true,\"currentOdsCode\":\"currentOdsCode\",\"managingOrganisation\":\"managingOrganisation\",\"recordETag\":\"etag\",\"isDeceased\":false}";
+        return new ResponseEntity<>(pdsResponseString, HttpStatus.OK);
+    }
+}
