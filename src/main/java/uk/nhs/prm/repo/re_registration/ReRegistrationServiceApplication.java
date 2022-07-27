ea6c35399fb2b3aebb4636408e7ab356b9fd7fcf
Mario Giampietri <mario.giampietri1@nhs.net>
2022-07-27 17:47:52 +0100

    [PRMT-2658] refactoring - merged 2 tiny methods and modified a few log entries for improved readability

--- src/main/java/uk/nhs/prm/repo/re_registration/ReRegistrationServiceApplication.java
+++ src/main/java/uk/nhs/prm/repo/re_registration/ReRegistrationServiceApplication.java
@@ -11,5 +11,4 @@ public class ReRegistrationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReRegistrationServiceApplication.class, args);
 	}
-
 }
--- src/main/java/uk/nhs/prm/repo/re_registration/handlers/ReRegistrationsHandler.java
+++ src/main/java/uk/nhs/prm/repo/re_registration/handlers/ReRegistrationsHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import uk.nhs.prm.repo.re_registration.config.ToggleConfig;
-import uk.nhs.prm.repo.re_registration.ehr_repo.EhrDeleteResponseContent;
 import uk.nhs.prm.repo.re_registration.ehr_repo.EhrRepoService;
 import uk.nhs.prm.repo.re_registration.ehr_repo.IntermittentErrorEhrRepoException;
 import uk.nhs.prm.repo.re_registration.message_publishers.ReRegistrationAuditPublisher;
@@ -29,39 +28,35 @@ public class ReRegistrationsHandler {
     private final EhrRepoService ehrRepoService;
 
     public void process(String payload) {
-
         log.info("RECEIVED: Re-registrations Event Message, payload length: " + payload.length());
         var reRegistrationEvent = parser.parse(payload);
 
         if (toggleConfig.canSendDeleteEhrRequest()) {
-            checkSuspendedStatus(reRegistrationEvent);
+            log.info("Toggle canSendDeleteEhrRequest is true: processing event to delete ehr");
+            processReRegistration(reRegistrationEvent);
         } else {
             log.info("Toggle canSendDeleteEhrRequest is false: not processing event, sending update to audit");
             sendAuditMessage(reRegistrationEvent, "NO_ACTION:RE_REGISTRATION_EVENT_RECEIVED");
         }
     }
 
-    private void handlePdsResponse(ReRegistrationEvent reRegistrationEvent, boolean isSuspended) {
-        if (isSuspended) {
-            log.info("Patient is suspended");
-            sendAuditMessage(reRegistrationEvent, "NO_ACTION:RE_REGISTRATION_FAILED_STILL_SUSPENDED");
-        } else {
-            log.info("Patient is not suspended");
-            deleteEhr(reRegistrationEvent);
-        }
-    }
-
-    private void checkSuspendedStatus(ReRegistrationEvent reRegistrationEvent) {
+    private void processReRegistration(ReRegistrationEvent reRegistrationEvent) {
         try {
-            var parsedPdsAdaptorResponse = pdsAdaptorService.getPatientPdsStatus(reRegistrationEvent);
-            handlePdsResponse(reRegistrationEvent, parsedPdsAdaptorResponse.isSuspended());
+            log.info("Invoking pds to check patient status...");
+            var pdsAdaptorResponse = pdsAdaptorService.getPatientPdsStatus(reRegistrationEvent);
+            if (pdsAdaptorResponse.isSuspended()) {
+                log.info("Patient is suspended, no need to invoke ehr repo to delete records");
+                sendAuditMessage(reRegistrationEvent, "NO_ACTION:RE_REGISTRATION_FAILED_STILL_SUSPENDED");
+                return;
+            }
+            log.info("Patient is not suspended, going ahead invoking ehr repo to delete records");
+            deleteEhr(reRegistrationEvent);
         } catch (HttpStatusCodeException e) {
             handlePdsErrorResponse(reRegistrationEvent, e);
         }
     }
 
     private void handlePdsErrorResponse(ReRegistrationEvent reRegistrationEvent, HttpStatusCodeException e) {
-
         if (e.getStatusCode().is4xxClientError()) {
             log.info("Encountered client error with status code : {}", e.getStatusCode());
             sendAuditMessage(reRegistrationEvent, "NO_ACTION:RE_REGISTRATION_FAILED_PDS_ERROR");
@@ -70,14 +65,11 @@ public class ReRegistrationsHandler {
             log.info("Encountered server error with status code : {}", e.getStatusCode());
             throw new IntermittentErrorPdsException("Encountered error when calling pds get patient endpoint", e);
         }
-
     }
 
     private void deleteEhr(ReRegistrationEvent reRegistrationEvent) {
-        log.info("Toggle canSendDeleteEhrRequest is true: processing event to delete ehr");
-        EhrDeleteResponseContent ehrDeleteResponse;
         try {
-            ehrDeleteResponse = ehrRepoService.deletePatientEhr(reRegistrationEvent);
+            var ehrDeleteResponse = ehrRepoService.deletePatientEhr(reRegistrationEvent);
             sendAuditMessage(reRegistrationEvent, "ACTION:RE_REGISTRATION_EHR_DELETED with conversationIds: " + ehrDeleteResponse.getConversationIds());
         } catch (HttpStatusCodeException e) {
             handleEhrRepoErrorResponse(reRegistrationEvent, e);
@@ -87,20 +79,16 @@ public class ReRegistrationsHandler {
     }
 
     private void handleEhrRepoErrorResponse(ReRegistrationEvent reRegistrationEvent, HttpStatusCodeException e) {
-
         if (e.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
             log.info("Encountered client error with status code : {}", e.getStatusCode());
-            auditPublisher.sendMessage(new NonSensitiveDataMessage(reRegistrationEvent.getNemsMessageId(),
-                    "NO_ACTION:RE_REGISTRATION_EHR_NOT_IN_REPO"));
+            auditPublisher.sendMessage(new NonSensitiveDataMessage(reRegistrationEvent.getNemsMessageId(), "NO_ACTION:RE_REGISTRATION_EHR_NOT_IN_REPO"));
         } else if (e.getStatusCode().equals(HttpStatus.BAD_REQUEST)) {
             log.info("Encountered client error with status code : {}", e.getStatusCode());
-            auditPublisher.sendMessage(new NonSensitiveDataMessage(reRegistrationEvent.getNemsMessageId(),
-                    "NO_ACTION:RE_REGISTRATION_EHR_FAILED_TO_DELETE"));
+            auditPublisher.sendMessage(new NonSensitiveDataMessage(reRegistrationEvent.getNemsMessageId(), "NO_ACTION:RE_REGISTRATION_EHR_FAILED_TO_DELETE"));
         } else if (e.getStatusCode().is5xxServerError()) {
             log.info("Encountered server error with status code : {}", e.getStatusCode());
             throw new IntermittentErrorEhrRepoException("Encountered error when calling ehr-repo DELETE patient endpoint", e);
         }
-
     }
 
     private void sendAuditMessage(ReRegistrationEvent reRegistrationEvent, String status) {
