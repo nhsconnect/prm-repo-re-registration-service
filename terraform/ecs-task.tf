fc8548bd448aa0f8fefc1f4ac98cce4d5328ee04
Shruthi Chandekar <shruthi.chandekar1@nhs.net>
2022-08-31 12:10:13 +0100

    [PRMT-2764] Added active-suspensions queue name to the environment variable.
    Co-authored-by: romanenhs <romane.castera1@nhs.net>

--- terraform/ecs-task.tf
+++ terraform/ecs-task.tf
@@ -9,6 +9,7 @@ locals {
     { name = "LOG_LEVEL", value = var.log_level },
     { name = "RE_REGISTRATIONS_QUEUE_NAME", value = aws_sqs_queue.re_registrations.name },
     { name = "RE_REGISTRATIONS_AUDIT_QUEUE_NAME", value = aws_sqs_queue.re_registration_audit_uploader.name },
+    { name = "ACTIVE_SUSPENSIONS_QUEUE_NAME", value = aws_sqs_queue.active_suspensions.name },
     { name = "PDS_ADAPTOR_SERVICE_URL", value = data.aws_ssm_parameter.pds_adaptor_service_url.value },
     { name = "PDS_ADAPTOR_AUTH_PASSWORD", value = data.aws_ssm_parameter.pds_adaptor_auth_password.value },
     { name = "RE_REGISTRATIONS_AUDIT_SNS_TOPIC_ARN", value = aws_sns_topic.re_registration_audit_topic.arn },
