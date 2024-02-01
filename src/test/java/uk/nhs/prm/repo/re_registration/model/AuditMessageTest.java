8cf6b3ec2da2444e7ce5f3c9920c44d61caa79da
martin-nhs <martin.taylor22@nhs.net>
2024-02-01 12:12:14 +0000

    [PRMT-4462] Unit test structure improvements, focusing on DRY code.

--- src/test/java/uk/nhs/prm/repo/re_registration/model/AuditMessageTest.java
+++ src/test/java/uk/nhs/prm/repo/re_registration/model/AuditMessageTest.java
@@ -2,17 +2,18 @@ package uk.nhs.prm.repo.re_registration.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static uk.nhs.prm.repo.re_registration.utility.TestDataUtility.UNESCAPED_HTML;
 
 class AuditMessageTest {
-    private static final String NEMS_MESSAGE_ID = "1234567890";
-
     @Test
     void Given_AuditMessageContainingUnescapedHtml_When_ToJsonStringIsCalled_Then_ReturnValueShouldNotContainUnescapedHtml() {
         // Given
+        final String nemsMessageId = UUID.randomUUID().toString();
         final AuditMessage auditMessage = new AuditMessage(
-                NEMS_MESSAGE_ID,
+                nemsMessageId,
                 UNESCAPED_HTML
         );
 
--- src/test/java/uk/nhs/prm/repo/re_registration/model/ReRegistrationEventTest.java
+++ src/test/java/uk/nhs/prm/repo/re_registration/model/ReRegistrationEventTest.java
@@ -3,21 +3,23 @@ package uk.nhs.prm.repo.re_registration.model;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static uk.nhs.prm.repo.re_registration.utility.TestDataUtility.NHS_NUMBER;
 import static uk.nhs.prm.repo.re_registration.utility.TestDataUtility.UNESCAPED_HTML;
+import static uk.nhs.prm.repo.re_registration.utility.TestDataUtility.getRandomOdsCode;
+import static uk.nhs.prm.repo.re_registration.utility.TestDataUtility.getRandomTimestamp;
 
 class ReRegistrationEventTest {
-    private static final String NHS_NUMBER = "1234567890";
-    private static final String ODS_CODE = "B74158";
-    private static final String TIMESTAMP = "2024-02-01T11:40:03+0000";
-
     @Test
     void Given_ReRegistrationEventContainingUnescapedHtml_When_ToJsonStringIsCalled_Then_ReturnValueShouldNotContainUnescapedHtml() {
         // Given
+        final String timestamp = getRandomTimestamp();
+        final String odsCode = getRandomOdsCode();
+
         final ReRegistrationEvent reRegistrationEvent = new ReRegistrationEvent(
                 NHS_NUMBER,
-                ODS_CODE,
+                odsCode,
                 UNESCAPED_HTML,
-                TIMESTAMP
+                timestamp
         );
 
         // When
